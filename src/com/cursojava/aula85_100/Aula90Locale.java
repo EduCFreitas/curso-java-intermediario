28e97ae359713e561886ab4875afc9fce7ea84a9
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-09-14 18:06:57 -0300

    Aula 90 - Locale

---
+++ src/com/cursojava/aula85_100/Aula90Locale.java
@@ -0,0 +1,45 @@
+package com.cursojava.aula85_100;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Aula90Locale {
+
+	public static void main(String[] args) {
+		
+		Locale locale = Locale.getDefault();
+		System.out.println(locale);
+		
+		System.out.println();
+		
+		Locale[] locales = Locale.getAvailableLocales();
+		for(Locale loc : locales) {
+			System.out.println("Nome: " + loc.getDisplayName());
+			System.out.println("Código da língua: " + loc.getLanguage());
+			System.out.println("Língua: " + loc.getDisplayLanguage());
+			System.out.println("Código do país: " + loc.getCountry());
+			System.out.println("País: " + loc.getDisplayCountry());
+			System.out.println("----------------------------------");
+		}
+		
+		System.out.println();
+		
+		//Setar Locale como padrão
+		Locale br = new Locale("pt", "Brazil");
+		System.out.println(br);
+		
+		Locale.setDefault(br);
+		System.out.println(Locale.getDefault());
+		
+		NumberFormat nf = NumberFormat.getCurrencyInstance();
+		System.out.println(nf.format(5000000000d));
+		
+		NumberFormat nf1 = NumberFormat.getCurrencyInstance(Locale.US);
+		System.out.println(nf1.format(5000000000d));
+		
+		NumberFormat nf2 = NumberFormat.getCurrencyInstance(Locale.ITALY);
+		System.out.println(nf2.format(5000000000d));
+
+	}
+
+}
