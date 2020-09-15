01e3dcc5ee2204d8e1c11f52cb95d7b6bfe7783c
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-09-15 17:52:29 -0300

    Aula 92 - TimeZone (fuso horário)

---
+++ src/com/cursojava/aula85_100/Aula92TimeZone.java
@@ -0,0 +1,44 @@
+package com.cursojava.aula85_100;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class Aula92TimeZone {
+
+	public static void main(String[] args) {
+		
+		Calendar calendar = Calendar.getInstance();
+		TimeZone tz = calendar.getTimeZone();
+		
+		System.out.println(tz);
+		
+		//Fusos-horários disponíveis no Java
+		String[] fusos = TimeZone.getAvailableIDs();
+		for(String fuso : fusos) {
+			System.out.println(fuso);
+		}
+		
+		System.out.println();
+		
+		//Configurar TimeZone padrão
+		TimeZone tzSP = TimeZone.getTimeZone("America/Sao_Paulo");
+		
+		System.out.println(tzSP.getDisplayName());
+		System.out.println(tzSP.getID());
+		
+		//Converter datas e salvar
+		Calendar agora = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss a z");
+		System.out.println(sdf.format(agora.getTime()));
+		
+		Calendar agoraSP = Calendar.getInstance(tzSP);
+		System.out.println(agoraSP.getTimeZone());
+		System.out.println(sdf.format(agoraSP.getTime()));
+		
+		agoraSP.add(Calendar.HOUR_OF_DAY, tzSP.getOffset((System.currentTimeMillis()))/1000/60/60);
+		System.out.println(sdf.format(agoraSP.getTime()));
+
+	}
+
+}
