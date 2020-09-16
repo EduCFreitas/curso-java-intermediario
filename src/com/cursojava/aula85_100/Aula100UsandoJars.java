f7a01ff32bc6d8a9fd6bb3c7e2e72eacb582cb46
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-09-16 17:35:58 -0300

    Aula 100 - Instalando Jars no projeto

---
+++ src/com/cursojava/aula85_100/Aula100UsandoJars.java
@@ -0,0 +1,19 @@
+package com.cursojava.aula85_100;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Aula100UsandoJars {
+
+	public static void main(String[] args) {
+		
+		//Baixar arquivo bin do jar (ex: https://commons.apache.org/)
+		//Copiar o arquivo .jar
+		//Criar pasta lib no projeto
+		//Colar arquivo .jar
+		//Através do Eclipse, botão direito no arquivo .jar, Build Pah, Add to Build Path
+		
+		System.out.println(StringUtils.capitalize("eduardo"));
+
+	}
+
+}
